cd3d25ad441a19987067853971efb6749d8a1b74
garv-2501 <garvsinghrawlot1.0@gmail.com>
2022-12-11 10:42:11 +0000

    2 basic extensions with some incompete styling.

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
 // A web application to provide discussion forums
 
 // Import the modules we need
-var express = require ('express')
-var ejs = require('ejs')
-var bodyParser= require ('body-parser')
-const mysql = require('mysql');
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 // Create the express application object
-const app = express()
-const port = 8000
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define the database connection
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'forumapp',
-    password: 'qwerty',
-    database: 'myforum'
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "forumapp",
+    password: "qwerty",
+    database: "myforum",
 });
 // Connect to the database
 db.connect((err) => {
     if (err) {
         throw err;
     }
-    console.log('Connected to database');
+    console.log("Connected to database");
 });
 global.db = db;
 
@@ -33,20 +33,20 @@ app.use(express.static(__dirname + "/public"));
 
 // Set the directory where Express will pick up HTML files
 // __dirname will get the current directory
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 
 // Tell Express that we want to use EJS as the templating engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Tells Express how we should process html files
 // We want to use EJS's rendering engine
-app.engine('html', ejs.renderFile);
+app.engine("html", ejs.renderFile);
 
 // Define our data
-var forumData = {forumName: "Fruitnet Forums"}
+var forumData = { forumName: "Frutchan" };
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, forumData);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
